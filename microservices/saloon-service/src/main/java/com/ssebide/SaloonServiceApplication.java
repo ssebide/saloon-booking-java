c375c4dd57db76458d558351320311bb267a6318
ssebide <sebiddejohn136@gmail.com>
2025-03-08 09:07:43 +0300

    Folder Reorganization

---
+++ microservices/saloon-service/src/main/java/com/ssebide/SaloonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ssebide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaloonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaloonServiceApplication.class, args);
+	}
+
+}
